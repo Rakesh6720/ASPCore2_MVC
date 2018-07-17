e27c2d1d458f13b22a4fb79736ad7084cf2b7bee
Rakesh Baruah <rbaruah@gmail.com>
2018-07-17 18:47:30 -0500

    secondCommit

---
+++ ContosoUniversity/ContosoUniversity/Models/Enrollment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Models
+{
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+        public Grade? Grade { get; set; } //Grade is nullable enum
+
+        public Course Course { get; set; } //Nav Prop
+        public Student Student { get; set; } //Nav Prop
+    }
+}
---
+++ ContosoUniversity/ContosoUniversity/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        //Nav Property below is a 1-to-Many relationship, hence LIST
+        //EF creates HashSet by default for ICollection<T>
+        public ICollection<Enrollment> Enrollments { get; set; } //Navigation Property
+    }
+}

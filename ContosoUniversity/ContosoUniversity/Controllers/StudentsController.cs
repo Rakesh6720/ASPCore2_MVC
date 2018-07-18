4f1762967d905a0b4c0b9167fbcb42c02945493d
Rakesh Baruah <rbaruah@gmail.com>
2018-07-17 19:54:15 -0500

    Up through students controller/view.

--- ContosoUniversity/ContosoUniversity/Controllers/StudentsController.cs
+++ ContosoUniversity/ContosoUniversity/Controllers/StudentsController.cs
@@ -32,8 +32,11 @@ namespace ContosoUniversity.Controllers
             {
                 return NotFound();
             }
-
+            //Must display contents of a collection in HTML table
             var student = await _context.Students
+                .Include(s => s.Enrollments)
+                    .ThenInclude(e => e.Course)
+                .AsNoTracking()
                 .SingleOrDefaultAsync(m => m.ID == id);
             if (student == null)
             {
@@ -46,6 +49,7 @@ namespace ContosoUniversity.Controllers
         // GET: Students/Create
         public IActionResult Create()
         {
+
             return View();
         }
 
@@ -54,13 +58,21 @@ namespace ContosoUniversity.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ID,LastName,FirstMidName,EnrollmentDate")] Student student)
+        public async Task<IActionResult> Create([Bind("LastName,FirstMidName,EnrollmentDate")] Student student)
         {
-            if (ModelState.IsValid)
+            try
             {
-                _context.Add(student);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                if (ModelState.IsValid)
+                {
+                    _context.Add(student);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            catch (DbUpdateException /* ex */)
+            {
+                ModelState.AddModelError("", "Unable to save changes. " + "Try again, and if the problem persists " +
+                    "see your system administrator.");
             }
             return View(student);
         }
